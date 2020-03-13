0f018a64b63308c5e1d51f123ff1f291bc7ee3a4
Raxa Hansana <raxah@Raxas-MacBook-Pro.local>
2020-03-13 15:42:20 -0700

    final

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,6 +1,11 @@
 const products = require("../products.json");
 
 const getProducts = (req, res) => {
+  if (req.query.price) {
+    const items = products.filter(
+      val => val.price >= parseInt(req.query.price)
+    );
+  }
   res.status(200).send(products);
 };
 
